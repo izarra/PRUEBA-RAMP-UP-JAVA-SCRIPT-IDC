678c073ef5ec089668b160c4b99e854b29e72fd0
IDCIzarraDomingoCansino <31973220+izarra@users.noreply.github.com>
2023-06-01 10:06:38 +0200

    Add files via upload
    En MAYUSCULA EJERCICIO 1, en minuscula ejercicio 2

---
+++ main.js
@@ -0,0 +1,4 @@
+/*Ejercicio 2:
+Diseña un algoritmo que imprima los números impares entre un número dado por el usuario y los siguientes 50 números
+*/
+const arraNumerosPares=[1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49]
